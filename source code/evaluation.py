0eb6f3f8cea2a5ecccd17b523e74dd1ddc8a36cf
Xing Hu <huxing0101@pku.edu.cn>
2021-02-18 16:18:20 +0800

    remove corpus bleu score computed by nltk

--- source code/evaluation.py
+++ source code/evaluation.py
@@ -1,14 +1,7 @@
 import nltk
 from nltk.translate.bleu_score import SmoothingFunction
 
-def nltk_sentence_bleu(hypothesis, reference, order=4):
-    if len(hypothesis) < order:
-        return 0
-    else:
-        return nltk.translate.bleu([reference], hypothesis)
-
-
-def nltk_corpus_bleu(hypotheses, references, order=4):
+def nltk_sentence_bleu(hypotheses, references, order=4):
     refs = []
     count = 0
     total_score = 0.0
@@ -24,6 +17,5 @@ def nltk_corpus_bleu(hypotheses, references, order=4):
             total_score += score
             count += 1
     avg_score = total_score / count
-    corpus_bleu = nltk.translate.bleu_score.corpus_bleu(refs, hypotheses, smoothing_function=cc.method4)
-    print('corpus_bleu: %.4f avg_score: %.4f' % (corpus_bleu, avg_score))
-    return corpus_bleu, avg_score
+    print('avg_score: %.4f' % avg_score)
+    return avg_score
